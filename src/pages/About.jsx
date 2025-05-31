2cfcb02eb2c2785ae5574686ebcdb6796800f64d
vynas <hamdabousnina@gmail.com>
2025-05-31 19:06:49 -0400

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <Link to="/">Home Page</Link>
-    </div>
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt excepturi
+        molestias quos voluptates labore minima ratione nesciunt, a tempore
+        nemo, cumque quod eius alias quis harum laboriosam explicabo. Ex, iste.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloremque
+        tempore quaerat autem molestias expedita aliquam aliquid natus
+        exercitationem est quasi labore illo voluptatum, sint ipsum minima
+        dolorum neque sunt. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Neque minima quae culpa mollitia distinctio quidem odit vel
+        laudantium sapiente molestiae, sed consectetur dolor accusamus impedit
+        dignissimos aperiam laboriosam animi consequuntur.
+      </p>
+    </Wrapper>
   );
 };
 export default About;
