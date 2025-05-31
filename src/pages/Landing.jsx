c482d22593b997d7a9b8505373fc163eb85ae076
vynas <hamdabousnina@gmail.com>
2025-05-31 19:46:54 -0400

    fix loader import

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  return "something";
+};
+
 const Landing = () => {
+  const data = useLoaderData();
   return <h1>Landing</h1>;
 };
 export default Landing;
