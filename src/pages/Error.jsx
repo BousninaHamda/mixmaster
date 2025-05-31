6db01607a4cf6ddcf8da9974cc3139bacf1efac0
vynas <hamdabousnina@gmail.com>
2025-05-31 19:19:03 -0400

    Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,25 @@
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
+
 const Error = () => {
-  return <h1>Error</h1>;
+  const error = useRouteError();
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>ohh!</h3>
+          <p>Wa can't seem to find the page you are looking for</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h3>Something Went Wrong</h3>;
+    </Wrapper>
+  );
 };
 export default Error;
