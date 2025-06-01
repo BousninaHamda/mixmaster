90df32714529ecb2cbc7fe01b4d51b367f4c03d7
vynas <hamdabousnina@gmail.com>
2025-06-01 19:08:50 -0400

    add the ingredients of each cocktail

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -23,6 +23,12 @@ const Cocktail = () => {
     strInstructions: instructions,
   } = singleDrink;
 
+  const ingredients = Object.keys(singleDrink)
+    .filter(
+      (key) => key.startsWith("strIngredient") && singleDrink[key] !== null
+    )
+    .map((key) => singleDrink[key]);
+
   return (
     <Wrapper>
       <header>
@@ -49,6 +55,17 @@ const Cocktail = () => {
           <p>
             <span className="drink-data">glass :</span>
             {glass}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {ingredients.map((item, index) => {
+              return (
+                <span className="ing" key={item}>
+                  {item}
+                  {index < ingredients.length - 1 ? "," : ""}
+                </span>
+              );
+            })}
           </p>
           <p>
             <span className="drink-data">instructions :</span>
