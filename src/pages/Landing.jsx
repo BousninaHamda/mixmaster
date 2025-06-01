2270c68328cb4cfb7895b21fcd259e5ddf38282c
vynas <hamdabousnina@gmail.com>
2025-05-31 20:22:03 -0400

    CocktailList component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,22 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import CocktailList from "../components/CocktailList";
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const loader = async () => {
-  const searchTerm = "margarita";
+  const searchTerm = "all";
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
-  return <h1>Landing</h1>;
+  return (
+    <>
+      <CocktailList drinks={drinks} />
+    </>
+  );
 };
 export default Landing;
