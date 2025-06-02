659e628d8d36345f04c5d34e827d662db4a16e9c
vynas <hamdabousnina@gmail.com>
2025-06-01 20:10:41 -0400

    newsletter structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <ToastContainer position="top-center" autoClose={2000} />
+    <App />
+  </>
+);
