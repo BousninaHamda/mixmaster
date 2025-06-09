0101ed312016a9973c16b14e386528729030e47f
vynas <hamdabousnina@gmail.com>
2025-06-09 09:42:56 -0400

    search form

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async () => {
-  const searchTerm = "a";
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get("search") || "a";
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: response.data.drinks, searchTerm };
 };
@@ -15,6 +17,7 @@ const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
   return (
     <>
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </>
   );
